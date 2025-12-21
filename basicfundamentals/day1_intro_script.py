209004e120aecef50e1873ef9c09dc666452d726
varshababu26 <varshababu26@gmail.com>
2025-12-21 21:51:38 +0530

    Update day1_intro_script.py

--- basicfundamentals/day1_intro_script.py
+++ basicfundamentals/day1_intro_script.py
@@ -1,1 +1,11 @@
+# Enter the user's name
+name = input("Enter your name: ")
+
+# Enter current role
+role = input("Please enter your current position: ")
+
+# Enter years of experience
+years_of_exp = input("Years of experience: ")
+
+print(f"Your name is {name}. Current role is {role} and years of experience is {years_of_exp}.")
 
